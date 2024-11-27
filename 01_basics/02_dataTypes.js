4a3a96674c8baf797cf7d057175c46a51784ef67
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-27 15:37:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";  //treat all JS code as newer version (newer JS)
+
+// alert(3 + 3)  //we are using node.js, not browser
+
+console.log(3 
+    +
+    3) // code readability should be high
+
+console.log("Riya")
+
+
+let name = "riya"
+let age = 28
+let isLoggedIn = false
+let state; //undefined
+let number = null //empty value
+
+//     primitive data type
+// number => 2 to the power of 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>  
+// symbol => unique
+
+
+//     object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); // object
+
+
