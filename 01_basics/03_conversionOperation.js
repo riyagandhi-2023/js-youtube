0ecc913b737022174fd7c30d4aa0eb4702542897
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-27 16:05:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "riya"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33aabc" => NaN (Not a Number)
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+// "riya" => NaN
+
+
+let isLoggedIn = "riya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "riya" => ture
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
