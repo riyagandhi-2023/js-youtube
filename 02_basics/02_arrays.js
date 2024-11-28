013b02cf476bc0fdfb8b4ac536ab59011f22cc96
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-28 02:47:36 +0000

    arrays part 1

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,43 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros);        // Won't merge but it will be in array inside array
+
+// console.log(marvel_heros);           // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1]);
+
+// concat:  Combine two or more arrays. This method returns a new arrya without modifying any existing arrys
+
+// const allHeros = marvel_heros.concat(dc_heros)          
+// console.log(allHeros);                  // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+//Spread operator: 
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+// flat: returns a new array with all sub-array elements concatenated into it recursively up t the specified depth
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("Riya"));
+console.log(Array.from("Riya"));            //convert into array
+console.log(Array.from({name: "riya"}));   // will give you empty array
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));          // .of will return a new array from a set of elements
+
+
+
+
+
