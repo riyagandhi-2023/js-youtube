cc0af6cb7ac94f0d560fe16a9462b125ad7808ef
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-28 02:47:57 +0000

    array part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,27 +1,22 @@
 // array
 // Array is collection of multiple items under a single variable name
-// JS arrays are resizable and can contain a mixof different data types: const myArr = [1, 1, 2, 3, 4, 5, true, "riya"] 
+// JS arrays are resizable and can contain a mixof different data types: const myArr = [1, 1, 2, 3, 4, 5, true, "riya"]
 // inside bracket it is called elements
 // JS arrays are not associative arrays, cannot access: console.log(myArr["one"]);
 // must be accessed using nonnegative integers (or their respective string form) as indexes: console.log(myArr[0]);
 // zero based indexed
 
-
 // shallow copy is like heap
 // deep copy is like stack
 
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["shaktiman", "naagraj"];
 
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeros = ["shaktiman", "naagraj"]
+const myArr2 = new Array(1, 2, 3, 4);
 
-const myArr2 = new Array(1, 2, 3, 4)
-
-// console.log(myArr[1]); 
-
-
+// console.log(myArr[1]);
 
 //Array methods
-
 
 // myArr.push(6)               // .push() add value in the last in array
 // myArr.push(7)
@@ -36,13 +31,11 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr.indexOf(19));         // can ask for index: if the value is not in the array it will show -1 value for the index
 // console.log(myArr.indexOf(3));
 
-
- const newArr = myArr.join()
+const newArr = myArr.join();
 
 // console.log(myArr);          //typeof object
 // console.log(newArr);         //typeof string
 // console.log(typeof newArr);
-
 
 // slice, splice
 
@@ -50,28 +43,14 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A", myArr);
 
-const myn1 = myArr.slice(1,3)
+const myn1 = myArr.slice(1, 3);
 
 console.log(myn1);
 console.log("B", myArr);
 
-
 // splice: manipulate original array
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3);
 
 console.log("C", myArr);
 console.log(myn2);
-
-
-
-
-
-
-
-
-
-
-
-
-
