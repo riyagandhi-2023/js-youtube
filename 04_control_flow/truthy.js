d978943d5ed7ac7366832cba597d8c2ea7e042e9
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-29 04:24:57 +0000

    code formatted

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -64,18 +64,15 @@ let val1;
 // val1 = 5 ?? 10                   // output: 5
 // val1 = null ?? 10                // output: 10
 // val1 = undefined ?? 15           // output: 15
-val1 = null ?? 10 ?? 15             // output: 10
-
+val1 = null ?? 10 ?? 15; // output: 10
 
 // first value that it gets it is the output
 
 console.log(val1);
 
-
 // Terniary Operator
 
 // condition ? true : false
 
-const iceTeaPrice = 100
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
-
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
