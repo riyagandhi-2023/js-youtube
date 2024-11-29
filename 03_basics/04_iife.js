9c1fcc7e0f35228d08ac6ad2e1e169a9cd4b3c10
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-29 00:46:59 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,54 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+/*
+// Why we use Immediately Invoked Function Expressions (IIFE)
+ => Answer
+Sometimes we face problems due to global scope pollution.  
+To remove whatever variable or declaration of global scope pollution we use IIFE.  */ 
+
+/* BEST WAY TO ANSWER:
+
+"Sometimes we face problems due to global scope pollution, where variables or declarations unintentionally interfere with other parts of the code. 
+To address this, we use Immediately Invoked Function Expressions (IIFEs), which create their own scope and execute immediately. 
+This prevents variables and functions defined inside the IIFE from polluting the global scope, ensures encapsulation, and avoids name collisions."
+
+*/
+
+// ------ // ({function})(execution);// ----//
+// need to END with ; (semi-colon)
+
+
+// named IIFE
+(function chai(){
+   // console.log(`DB CONNECTED`);
+}) ();
+
+
+( function aurcode() {
+   // console.log(`DB CONNECTED THREE`);
+}) (); 
+
+
+// Un-named or simple IIFE
+( () => {
+    //Un-named IIFE
+    //console.log(`DB CONNECTED TWO`);
+}) ();
+
+
+// how to pass parameter in IIFE
+( (name) => {
+    //console.log(`DB CONNECTED THREE ${name}`);
+}) ('riya');
+
+// how to write two IIFE together?
+
+(function () {
+    console.log("IIFE 1 is executed!");
+})();
+
+(function () {
+    console.log("IIFE 2 is executed!");
+})();
+
+
