0b61158b8678c3f11959a34932a73544efcef2b1
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-29 00:17:22 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,6 +1,8 @@
 // you cannot use this in arrow function
 // this is refer to current context
 
+// lines 5 to 16 username is a property of the user object, so this.username works. 
+
 const user = {
     username: "riya",
     price: 999,
@@ -27,31 +29,81 @@ const user = {
 
 /*  
 => when you run this in function in node environment you will get many values that you can access
+=> this is determined by how the function is called. 
+   When called as a standalone function, it defaults to the global context (or undefined in strict mode).
 */
 
-/*
-function chai(){
-    // console.log(this);                   // this context (this) only work in object
 
-    let username = "riya"
-    console.log(this.username);             //output: undefined;  this context (this.username) cannot work in a function;
+
+// function chai(){
+//     // console.log(this);                   // this context (this) only work in object
+
+//     let username = "riya"
+//     console.log(this.username);             //output: undefined;  this context (this.username) cannot work in a function;
     
+// }
+
+// chai()
+
+
+// username is a local variable inside the function, not a property of this
+
+
+// const chai = function (){
+//     let username = "riya"
+//     //console.log(this.username);                 output: undefined
+    
+// }
+
+// chai()
+
+
+
+// ===================== this in Arrow function =================
+
+
+const chai = () => {
+    let username = "riya"
+    //console.log(this.username);                 // output: undefined
+    //console.log(this);                          // output: {} empty
 }
 
 chai()
 
-*/
-
-
-/*
-const chai = function (){
-    let username = "riya"
-    //console.log(this.username);                 output: undefined
-    
-}
-
-chai()
-*/
 
 
 // ===================== Arrow function =================
+
+// ===> Explicit return (basic) arrow function
+
+// ** if you wrapping in {} curly braces [=() => {}] need to write "retrun" **//
+// follow below example
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3,4));
+
+//---------------------------------------------//
+
+// ===> Implicit return arrow function
+
+// ** if you are not wrapping or using paratheses () [= () => ] OR  [= () => ()] no need to write "return" **//
+// follow below examples
+
+//const addTwo = (num1, num2) =>  num1 + num2
+//console.log(addTwo(3,4));
+
+// const addTwo = (num1, num2) =>  (num1 + num2)
+// console.log(addTwo(3,4));
+
+
+const addTwo = (num1, num2) =>  ({username: "riya"})        // to return object need to wrap in paratheses ({ })
+//console.log(addTwo(3,4));
+
+
+//---------------------------------------------//
+
+// const myArray = [2, 5, 3, 7, 8]
+// myArray.forEach()
