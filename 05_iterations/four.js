0e71d73b2f58f0a9197b211d22c3c399ddbba9f6
Riya Gandhi <126266882+riyagandhi-2023@users.noreply.github.com>
2024-11-29 20:49:46 +0000

    for-in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,36 @@
+// for-in loop
+
+
+// object can be iterable using for-in
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+   // console.log(programming[key]);
+    
+}
+
+
+//======================== Map is not iterable ===============
+
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("Fr", "France");
+map.set("IN", "India");
+
+for (const key in map) {
+    console.log(map[key]);
+}
